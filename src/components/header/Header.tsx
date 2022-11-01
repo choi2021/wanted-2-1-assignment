b2c47cf588afd6e2b6cf3f80d30f31e918758b3e
Youngjun Choi <juni2004c@gmail.com>
2022-11-02 02:41:57 +0900

    [feat] Header 컴포넌트 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { Link, useLocation } from 'react-router-dom';
+import S from './styles';
+
+enum Title {
+  Home = '전체차량',
+  Detail = '차량상세',
+}
+
+const HOME_URL = '/';
 
 const Header = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+  const isHome = location.pathname === HOME_URL;
+  const text = isHome ? Title.Home : Title.Detail;
+  return (
+    <S.Header>
+      {!isHome && (
+        <Link to="/">
+          <AiOutlineArrowLeft />
+        </Link>
+      )}
+      <span>{text}</span>
+    </S.Header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  box-sizing: border-box;
+  width: 100%;
+  text-align: center;
+  font-size: 1.3rem;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 2rem;
+  border-bottom: 2px solid ${(props) => props.theme.colors.black};
+  a {
+    font-size: 1.5rem;
+    transform: translateX(-550%);
+  }
+`;
+
+const S = { Header };
+
+export default S;
