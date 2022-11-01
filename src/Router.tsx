3a53e08a9e000b01a72b3f8eb35b622497a8b415
Youngjun Choi <juni2004c@gmail.com>
2022-11-02 01:45:52 +0900

    [setting] Router 세팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/detail/:id',
+        element: <Detail />,
+      },
+    ],
+  },
+]);
 
 const Router = () => {
-  return (
-    <div>
-      <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
