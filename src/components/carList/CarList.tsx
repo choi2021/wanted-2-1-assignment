61e1ae3f938d72aec8077cb015d0c937937e8680
Youngjun Choi <juni2004c@gmail.com>
2022-11-02 17:19:13 +0900

    [design] CarList UI 추가

--- src/components/carList/CarList.tsx
+++ src/components/carList/CarList.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import CarItem from '../carItem/CarItem';
+import { useCarsState } from '../../hooks/useCars';
+
+const Layout = styled.section`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const CarList = () => {
-  return <ul>CarList</ul>;
+  const { data, isLoading } = useCarsState();
+  if (isLoading) {
+    return (
+      <Layout>
+        <h3>불러오는 중</h3>
+      </Layout>
+    );
+  }
+
+  if (data.length === 0) {
+    return (
+      <Layout>
+        <h3>차량이 없습니다.</h3>
+      </Layout>
+    );
+  }
+  return (
+    <ul>
+      {data.map((car) => (
+        <CarItem />
+      ))}
+    </ul>
+  );
 };
 
 export default CarList;
