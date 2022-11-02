d8ca30a4fb8e41cd8960bd692d87a39446fd4356
Youngjun Choi <juni2004c@gmail.com>
2022-11-02 15:21:54 +0900

    [refact] useReducer를 이용한 cars 상태관리 로직 Context API에 추가

--- src/context/carsContext.tsx
+++ src/context/carsContext.tsx
@@ -1,55 +1,75 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import { createContext, useContext, Dispatch, useReducer } from 'react';
+import { Cars } from '../interfaces/CarsInterface';
 
-enum Segment {
-  ENUMC = 'ENUMC',
-  D = 'D',
-  E = 'E',
-  SUV = 'SUV',
-}
-enum FuelType {
-  gasoline = 'gasoline',
-  ev = 'ev',
-  hybrid = 'hybrid',
-}
+const SET_IS_LOADING = 'SET_IS_LOADING';
+const SET_DATA = 'SET_DATA';
+const SET_ERROR = 'SET_ERROR';
 
-type Attribute = {
-  brand: string;
-  name: string;
-  segment: Segment;
-  fuelType: FuelType;
-  imageUrl: string;
+type State = {
+  isLoading: boolean;
+  data: Cars[];
+  error: string;
 };
 
-type Insurance = {
-  name: string;
-  description: string;
-};
-
-type AdditionalProducts = {
-  name: string;
-  amount: number;
-};
-
-interface ICars {
-  id: number;
-  amount: number;
-  attribute: Attribute;
-  startDate: Date;
-  createdAt: Date;
-  insurance?: Insurance[];
-  additionalProducts?: AdditionalProducts[];
+interface Action {
+  type: string;
+  data: Cars[];
+  error: string;
+  isLoading: boolean;
 }
 
-type CarsState = {
-  cars: ICars[];
+type CarsDistpatch = Dispatch<Action>;
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: '',
 };
 
-const CarsContext = createContext<CarsState | null>(null);
+const CarsStateContext = createContext<State | null>(initialState);
+const CarsDispatchContext = createContext<CarsDistpatch | null>(null);
 
-export const useCars = () => useContext(CarsContext);
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case SET_IS_LOADING:
+      return {
+        ...state,
+        isLoading: action.isLoading,
+      };
+    case SET_DATA:
+      return {
+        ...state,
+        data: action.data,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
+    default:
+      throw new Error('Unknown Action');
+  }
+};
+
+export const useCarsState = () => {
+  const state = useContext(CarsStateContext);
+  if (!state) throw new Error("Can't find State Provider");
+  return state;
+};
+
+export const useCarsDispatch = () => {
+  const dispatch = useContext(CarsDispatchContext);
+  if (!dispatch) throw new Error("Can't find Dispatch Provider");
+  return dispatch;
+};
 
 export const CarsProvider = ({ children }: { children: React.ReactNode }) => {
-  const [cars, setCars] = useState<ICars[]>([]);
-  const value = useMemo(() => ({ cars, setCars }), [cars]);
-  return <CarsContext.Provider value={value}>{children}</CarsContext.Provider>;
+  const [cars, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CarsStateContext.Provider value={cars}>
+      <CarsDispatchContext.Provider value={dispatch}>
+        {children}
+      </CarsDispatchContext.Provider>
+    </CarsStateContext.Provider>
+  );
 };
---
+++ src/interfaces/CarsInterface.ts
@@ -0,0 +1,39 @@
+enum Segment {
+  ENUMC = 'ENUMC',
+  D = 'D',
+  E = 'E',
+  SUV = 'SUV',
+}
+enum FuelType {
+  gasoline = 'gasoline',
+  ev = 'ev',
+  hybrid = 'hybrid',
+}
+
+type Attribute = {
+  brand: string;
+  name: string;
+  segment: Segment;
+  fuelType: FuelType;
+  imageUrl: string;
+};
+
+type Insurance = {
+  name: string;
+  description: string;
+};
+
+type AdditionalProducts = {
+  name: string;
+  amount: number;
+};
+
+export interface Cars {
+  id: number;
+  amount: number;
+  attribute: Attribute;
+  startDate: Date;
+  createdAt: Date;
+  insurance?: Insurance[];
+  additionalProducts?: AdditionalProducts[];
+}
